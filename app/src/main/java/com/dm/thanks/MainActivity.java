7794448e937a2757c1fd97001f64397f09c10f1f
Claudio Henrique <HacskBoy@hotmail.com>
2021-06-28 19:20:44 -0300

    feat: increments splash screen to app

--- app/src/main/java/com/dm/thanks/MainActivity.java
+++ app/src/main/java/com/dm/thanks/MainActivity.java
@@ -2,13 +2,30 @@ package com.dm.thanks;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+
+import com.dm.thanks.Activity.Login;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_splash_screen);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                showLogin();
+            }
+        }, 2000);
+    }
+
+    protected void showLogin(){
+        Intent intent = new Intent(MainActivity.this, Login.class);
+        startActivity(intent);
+        finish();
     }
 }
